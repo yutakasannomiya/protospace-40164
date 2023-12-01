453aaa13eb6d635d8d031458d0ce15b9875001ee
yutakasannomiya <151412157+yutakasannomiya@users.noreply.github.com>
2023-12-01 22:36:13 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,44 @@
+class PrototypesController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @prototypes = Prototype.all
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    Prototype.create(prototype_params)
+    redirect_to '/'
+  end
+
+  def show
+    @prototype = Prototype.find(params[:id])
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
+  end
+
+  def edit
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def update
+    prototype = Prototype.find(params[:id])
+    prototype.update(prototype_params)
+    redirect_to root_path
+  end
+
+  def destroy
+    prototype = Prototype.find(params[:id])
+    prototype.destroy
+    redirect_to root_path
+  end
+
+  private
+  def prototype_params
+    params.require(:prototype).permit(:title, :image, :catch_copy, :concept).merge(user_id: current_user.id)
+  end
+  
+end
---
+++ app/models/comment.rb
@@ -0,0 +1,6 @@
+class Comment < ApplicationRecord
+  validates :content, presence: true
+
+  belongs_to :prototype
+  belongs_to :user 
+end
