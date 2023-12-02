ca1e68e2aaf4be4a52eb903584646a218eb8e636
yutakasannomiya <151412157+yutakasannomiya@users.noreply.github.com>
2023-12-02 19:52:12 +0900

    修正６

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :password, length: { minimum: 6 }
 
   validates :name, presence: true
   validates :profile, presence: true
